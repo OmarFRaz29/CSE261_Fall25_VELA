c05864ff7197a008c899774df251ab537d9639df
OmarFRaz29 <omar.sghsc@gmail.com>
2026-01-14 22:32:17 +0600

    Leader File Added project.py

--- project.py
+++ project.py
@@ -2,16 +2,18 @@ print("Welcome to our project!")
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D 
-print("CTCS Vibrating String Simulation Started")
+
+print("CTCS Vibrating String Simulation Started") 
+L = 1.0; c = 1.0; T = 2.0 
+Nx = 50; Nt = 200 
 
 
 # Mamber 3 salman islam Tonmoy:
 x = np.linspace(0, L, Nx) 
 t = np.linspace(0, T, Nt) 
 u = np.zeros((Nt, Nx))
-
-
 
 
 # member 2 Fahim Islam Prithy:
@@ -30,6 +32,13 @@ u[1, 0] = 0.0
 u[1, -1] = 0.0
 
 
+for n in range(1, Nt - 1):
+    for i in range(1, Nx - 1):
+        u[n+1, i] = 2*u[n, i] - u[n-1, i] + r*(u[n, i+1] - 2*u[n, i] + u[n, i-1])
+    u[n+1, 0] = 0.0
+    u[n+1, -1] = 0.0
+
+
 
 
 # member 7 Rakibul Hasan Rakib:
@@ -45,6 +54,18 @@ for n in range(Nt - 1):
     energy[n] = ke + pe
 print("Initial Energy:", energy[0])
 print("Final Energy:", energy[Nt-2])
+
+#Omar:
+steps = [0, 40, 80, 120, 160, 199] 
+for s in steps: 
+     plt.figure(figsize=(8,4)) 
+     plt.plot(x, u[s], linewidth=3) 
+     plt.fill_between(x, 0, u[s], alpha=0.3) 
+     plt.title("Time = " + str(round(t[s], 3))) 
+     plt.xlabel("Position"); plt.ylabel("Displacement") 
+     plt.ylim(-0.15, 0.15); plt.grid(True) 
+     plt.show() 
+
 
 
 # member 9 Abyaj ahmed asif: 
